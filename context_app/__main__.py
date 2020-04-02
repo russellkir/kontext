7f7a6ce5a2efd010ac42007728c31c89b8627c38
Russell Kirmayer <russell.kirmayer@oden.io>
2020-04-02 11:27:24 -0400

    Checkpoint

--- context_app/__main__.py
+++ context_app/__main__.py
@@ -15,10 +15,10 @@ USER_ID = "MigGUNgKSFKsmATXRoB22A"
 def main():
     print("Kontext")
     client = zoom.ZoomClient(USER_ID)
-    # meeting = client.create_meeting()
-    # webbrowser.open(meeting["start_url"], new=2)
-    print(client.list_meetings())
-    print(client.list_live_meetings())
+    meeting = client.create_meeting()
+    webbrowser.open(meeting["start_url"], new=2)
+    # print(client.list_meetings())
+    # print(client.list_live_meetings())
 
 
 if __name__ == "__main__":
